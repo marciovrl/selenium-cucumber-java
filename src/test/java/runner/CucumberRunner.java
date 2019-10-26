85e9c304b7adb03aec0edf4a536b37e760d93e2b
marcio lemos <contato.marciovrl@gmail.com>
2019-10-25 21:12:32 -0300

    add @After test in CucumberRunner

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -1,18 +1,25 @@
 package runner;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import utils.BasePage;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = { "pretty", "html:reports/test-report" }, features = { "src/test/resources/" }, glue = {
-        "test/steps/definitions" }, tags = { "~@notImplemented" })
+@CucumberOptions(plugin = { "io.qameta.allure.cucumberjvm.AllureCucumberJvm" }, features = {
+        "src/test/resources/" }, glue = { "test/steps/definitions" }, tags = { "~@notImplemented" })
 
 public class CucumberRunner {
 
     @BeforeClass
     public static void tearUp() {
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        BasePage.quitBrowser();
     }
 }
