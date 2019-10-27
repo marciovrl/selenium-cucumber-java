e20e093c7e73b622d85b01fd468c26170102b2c2
marcio lemos <contato.marciovrl@gmail.com>
2019-10-27 13:04:34 -0300

    refac AfterClass

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -1,16 +1,16 @@
 package runner;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.java.After;
 import cucumber.api.junit.Cucumber;
 import utils.BasePage;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "io.qameta.allure.cucumberjvm.AllureCucumberJvm" }, features = {
-        "src/test/resources/" }, glue = { "test/steps/definitions" }, tags = { "~@notImplemented" })
+@CucumberOptions(features = { "src/test/resources/" }, glue = { "test/steps/definitions" }, tags = {
+        "~@notImplemented" })
 
 public class CucumberRunner {
 
@@ -18,7 +18,7 @@ public class CucumberRunner {
     public static void tearUp() {
     }
 
-    @After
+    @AfterClass
     public static void tearDown() {
         BasePage.quitBrowser();
     }
