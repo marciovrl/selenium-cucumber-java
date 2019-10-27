43858c800e75511de184d29a1c7504fc2ab96fa1
marcio lemos <contato.marciovrl@gmail.com>
2019-10-27 15:05:56 -0300

    add allure print

--- src/test/java/test/steps/Hooks.java
+++ src/test/java/test/steps/Hooks.java
@@ -1,14 +1,23 @@
 package test.steps;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
-import cucumber.api.java.Before;
+import io.qameta.allure.Allure;
 import utils.BasePage;
 
 public class Hooks {
 
     @After
-    public void afterScenario() {
+    public void afterScenario(Scenario scenario) {
+        byte[] screenshootBytes = ((TakesScreenshot) BasePage.getDriver()).getScreenshotAs(OutputType.BYTES);
+        InputStream screenshootStream = new ByteArrayInputStream(screenshootBytes);
+        Allure.addAttachment(scenario.getName(), screenshootStream);
         BasePage.quitBrowser();
     }
 }
