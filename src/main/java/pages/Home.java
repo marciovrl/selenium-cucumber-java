d37543d09e540f7348282ce257b429395bf07baa
marcio lemos <contato.marciovrl@gmail.com>
2020-04-30 11:33:06 -0300

    implemented using url properties

--- src/main/java/pages/Home.java
+++ src/main/java/pages/Home.java
@@ -3,15 +3,15 @@ package pages;
 import org.openqa.selenium.By;
 
 import utils.BasePage;
+import utils.Properties;
 
 public class Home extends BasePage {
 
-    private static final String URL_MERCADO_LIVRE = "https://www.mercadolivre.com.br/";
     private static final By INPUT_SEARCH = By.name("as_word");
     private static final By BUTTON_SEARCH = By.className("nav-search-btn");
 
     public void goToHomeMercadoLivre() {
-        openUrl(URL_MERCADO_LIVRE);
+        openUrl(Properties.URL_MERCADO_LIVRE);
     }
 
     public void searchItem(String item) {
