28e43f7cef76dee04ca39c5f3a1eb9e11eefcf4b
marcio lemos <contato.marciovrl@gmail.com>
2020-04-30 11:19:48 -0300

    created properties

---
+++ src/main/java/utils/Properties.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Properties {
+
+    public static String URL_MERCADO_LIVRE = "https://www.mercadolivre.com.br/";
+    public static Browsers browser = Browsers.CHROME;
+
+    public enum Browsers {
+        CHROME, CHROME_HEADLESS, FIREFOX
+    }
+}
